6a00201aba8310206ee885be17be6bb1a10e9531
arttturslv <arttturslv@gmail.com>
2024-05-31 13:37:48 -0300

    assets: added colors export

---
+++ constants/Colors.js
@@ -0,0 +1,12 @@
+
+export default Colors = {
+    FloralWhite: "F9F6EE",
+    Gunmetal: "2D3142",
+    Amethyst: "A663CC",
+    EerieBlack: "17181B",
+    Pumpkin: "FA8334",
+    BattleshipGray: "989898",
+    Jet: "292929",
+    Auburn: "A93535",
+    DavysGray: "5E5C55",
+}
