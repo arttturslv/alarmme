e7933e00504d2a50488b9fa89986a77ad58bd76a
arttturslv <arttturslv@gmail.com>
2024-07-22 17:12:16 -0300

    style: bottom navigation style

--- router/Tabs.routes.jsx
+++ router/Tabs.routes.jsx
@@ -5,17 +5,29 @@ import Alarm from "../screens/Alarm";
 import Stopwatch from "../screens/Stopwatch";
 import Timer from "../screens/Timer";
 import AlarmOptions from "../screens/AlarmOptions";
-
+import { Text } from "react-native";
 const Tab = createBottomTabNavigator();
 
 export default function Tabs() {
     return (
-        <Tab.Navigator>
+        <Tab.Navigator
+            screenOptions={{
+                headerShown: false,
+                tabBarIcon: ()=>{null},
+                tabBarStyle: {
+                    backgroundColor: Colors.Jet,
+                    borderTopWidth:0,
+                    justifyContent: 'center'
+                },
+                tabBarLabelStyle: {
+                    fontSize: 18,
+                    bottom: 12
+                }
+            }}
+        >
             <Tab.Screen name = 'Alarm' component={Alarm} />
             <Tab.Screen name = 'Stopwatch' component={Stopwatch} />
             <Tab.Screen name = 'Timer' component={Timer} />
-
-            <Tab.Screen name = 'AlarmOptions' component={AlarmOptions} />
 
         </Tab.Navigator>
     )
