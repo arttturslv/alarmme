f929c4095495e93fdc340e28a66542178762fc11
arttturslv <arttturslv@gmail.com>
2024-08-09 11:46:43 -0300

    fix: new date selection

--- components/Calendary.jsx
+++ components/Calendary.jsx
@@ -1,34 +1,47 @@
-import React, { useState } from "react";
-import { Button, View } from "react-native";
-import DateTimePickerModal from "react-native-modal-datetime-picker";
+import React, { useState} from "react";
+import {View, Pressable, Text, Image, StyleSheet } from "react-native";
 
-const Calendary = () => {
-  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+const Calendary = ({datesSelected, setDateSelected}) => {
 
-  const showDatePicker = () => {
-    setDatePickerVisibility(true);
-  };
 
-  const hideDatePicker = () => {
-    setDatePickerVisibility(false);
-  };
+  function switchDay(day) {
+    var daysSaved = [...datesSelected];
 
-  const handleConfirm = (date) => {
-    console.warn("A date has been picked: ", date);
-    hideDatePicker();
-  };
+    day = {...day, active: !day.active}
+
+    daysSaved = daysSaved.map(savedDay => 
+      savedDay.name === day.name ? day : savedDay // Substitui o objeto correspondente
+    );
+    setDateSelected(daysSaved)
+  }
 
   return (
-    <View>
-      <Button title="Show Date Picker" onPress={showDatePicker} />
-      <DateTimePickerModal
-        isVisible={isDatePickerVisible}
-        mode="date"
-        onConfirm={handleConfirm}
-        onCancel={hideDatePicker}
-      />
+    <View style={{flex:1}}>
+      <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+          <View style={{flexDirection:"row", gap: 6}}>
+            {
+              datesSelected.map((day) => {
+                return (
+                  <Pressable style={{paddingHorizontal: 3}} onPress={()=>switchDay(day)} >
+                    <Text Text style={day.active?{fontSize: 16, color: Colors.Amethyst}:{fontSize: 16, color: Colors.BattleshipGray}}>{day.name.substring(0,3)}</Text>
+                  </Pressable>
+                )
+              })
+            }
+          </View>
+          <Image style={styles.icon} source={require('../assets/icons/calendar.png')} />        
+      </View>
     </View>
   );
 };
 
 export default Calendary;
+
+const styles = StyleSheet.create({
+  icon: {
+      width:24, 
+      height:24, 
+      tintColor: Colors.BattleshipGray,
+      tintColor: '#989898'
+  }
+})
--- components/DatePicker.jsx
+++ components/DatePicker.jsx
@@ -1,20 +1,53 @@
 import { useState } from "react";
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
 import Colors from "../constants/Colors";
 import Switch from "./Switch";
+import Calendary from "../components/Calendary";
+
 export default function DatePicker() {
 
-    const [datesSelected, setDateSelected] = useState('s t q q s s d');
+    const [datesSelected, setDateSelected] = useState( [{
+        name: 'domingo',
+        active: false
+      },
+      {
+        name: 'segunda',
+        active: false
+      },
+      {
+        name: 'terça',
+        active: false
+      },
+      {
+        name: 'quarta',
+        active: false
+      },
+      {
+        name: 'quinta',
+        active: false
+      },
+      {
+        name: 'sexta',
+        active: false
+      },
+      {
+        name: 'sábado',
+        active: false
+      }]);
 
     return (
-        <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingHorizontal: 12}}>
-            <View>
-                <Text style={{fontSize: 18, color: Colors.BattleshipGray}}>{datesSelected}</Text>
-            </View>
-            <View style={{gap: 8, flexDirection: 'row', alignItems: 'center'}}>
-                <Image style={{width: 35, height: 35, tintColor: Colors.BattleshipGray }} source={{uri: 'https://pixsector.com/cache/0266455f/ava13c3ac53812cde1b88.png'}}/>
-                <Switch></Switch>
-            </View>
+        <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingHorizontal: 12, gap: 16}}>
+            <Calendary datesSelected={datesSelected} setDateSelected={setDateSelected}/>
         </View>
     )
 }
+
+
+const styles = StyleSheet.create({
+    icon: {
+        width:24, 
+        height:24, 
+        tintColor: Colors.BattleshipGray,
+        tintColor: '#989898'
+    }
+})
